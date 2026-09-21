458e1ef7d1e1e2acc1e44ba6e6f8b06104245c95
janmeermans <130315233+janmeermans@users.noreply.github.com>
2026-09-21 23:12:26 +0200

    Breek oude cache zodat de nieuwe versie laadt

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hartenjagen-v1";
+const CACHE = "hartenjagen-v2";
 const ASSETS = ["./", "./index.html", "./game.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
